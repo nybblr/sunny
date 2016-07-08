aac1b6a0a1aa0824e0e7af1008cd0b1a810f6b57
Jonathan Martin <me@nybblr.com>
2016-07-08 14:49:25 -0500

    Add search spec.

---
+++ spec/models/search_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Search do
+  subject(:search) { Search.new(query) }
+
+  let(:alice) { User.create(name: 'Alice', location: 'Anchorage') }
+  let(:bob) { User.create(name: 'Bob', location: 'Boston') }
+  let(:charlie) { User.create(name: 'Charlie', location: 'Chicago') }
+  let(:users) { [alice, bob, charlie] }
+
+  # Force user creation
+  let!(:data) { users }
+
+  describe 'searching for users' do
+    subject(:results) { search.users }
+    context 'when searching by location' do
+      let(:query) { 'Chicago' }
+      it { is_expected.to match_array [charlie] }
+    end
+  end
+end
